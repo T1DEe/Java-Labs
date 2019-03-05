6d3a65a9b43a8f8bcc25b0e6c56d355f44e62cd6
T1DEe <artemexe2013@gmail.com>
2019-03-05 22:23:41 +0300

    logger added

--- Java_Lab03/src/belstu/markovsky/Main.java
+++ Java_Lab03/src/belstu/markovsky/Main.java
@@ -3,17 +3,25 @@ package belstu.markovsky;
 import belstu.markovsky.car.CarHeavy;
 import belstu.markovsky.enums.WheelDrive;
 import belstu.markovsky.exception.CarException;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
 public class Main {
 
+    static {
+        new DOMConfigurator().doConfigure("log/log4j.xml",
+                LogManager.getLoggerRepository());
+    }
+    private static final Logger LOG = Logger.getLogger(Main.class);
+
+
     public static void main(String[] args) {
 
+        LOG.info("Program start...");
         try {
             CarHeavy carHeavy = new CarHeavy("Mercedes", 7000, 4000,
                     WheelDrive.FULL_DRIVE, 180, 9, 8000, 4000);
-
-
-
 
 
         } catch (CarException e) {
